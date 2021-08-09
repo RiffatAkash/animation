22147ad773c694319c66951dd6c73081b012588e
Riffat Akash <riffatakash@gmail.com>
2021-08-09 11:25:59 +0600

    Add files via upload

---
+++ big-pop-20/script.js
@@ -0,0 +1,68 @@
+var _0xdbbf = [".pop_ban_window", "querySelector", ".pop_ban_window_1", ".pop_ban_details_1", ".gp_feature_back_1", "opacity", "style", ".pop_ban_lines", "click", "top", "px", "addEventListener", ".pop_ban_window_2", ".pop_ban_details_2", ".gp_feature_back_2", ".pop_ban_window_3", ".pop_ban_details_3", ".gp_feature_back_3", ".pop_ban_window_4", ".pop_ban_details_4", ".gp_feature_back_4", ".pop_ban_window_5", ".pop_ban_details_5", ".gp_feature_back_5", ".pop_ban_window_6", ".pop_ban_details_6", ".gp_feature_back_6", ".pop_ban_window_7", ".pop_ban_details_7", ".gp_feature_back_7"];
+var allPop = document.querySelector('.pop_ban_window');
+var switch_1 = document.querySelector('.pop_ban_window_1');
+var details_1 = document.querySelector('.pop_ban_details_1');
+var back_1 = document.querySelector('.gp_feature_back_1');
+setTimeout(function() {
+    document.querySelector('.pop_ban_lines')['style']['opacity'] = 1
+}, 1e3);
+switch_1.addEventListener('click', function() {
+    details_1.style.top = 0 + 'px'
+});
+back_1.addEventListener('click', function() {
+    details_1.style.top = -250 + 'px'
+});
+var switch_2 = document.querySelector('.pop_ban_window_2');
+var details_2 = document.querySelector('.pop_ban_details_2');
+var back_2 = document.querySelector('.gp_feature_back_2');
+switch_2.addEventListener('click', function() {
+    details_2.style.left = 0 + 'px'
+});
+back_2.addEventListener('click', function() {
+    details_2.style.left = -300 + 'px'
+});
+var switch_3 = document.querySelector('.pop_ban_window_3');
+var details_3 = document.querySelector('.pop_ban_details_3');
+var back_3 = document.querySelector('.gp_feature_back_3');
+switch_3.addEventListener('click', function() {
+    details_3.style.top = 0 + 'px'
+});
+back_3.addEventListener('click', function() {
+    details_3.style.top = 250 + 'px'
+});
+var switch_4 = document.querySelector('.pop_ban_window_4');
+var details_4 = document.querySelector('.pop_ban_details_4');
+var back_4 = document.querySelector('.gp_feature_back_4');
+switch_4.addEventListener('click', function() {
+    details_4.style.top = 0 + 'px'
+});
+back_4.addEventListener('click', function() {
+    details_4.style.top = -250 + 'px'
+});
+var switch_5 = document.querySelector('.pop_ban_window_5');
+var details_5 = document.querySelector('.pop_ban_details_5');
+var back_5 = document.querySelector('.gp_feature_back_5');
+switch_5.addEventListener('click', function() {
+    details_5.style.left = 0 + 'px'
+});
+back_5.addEventListener('click', function() {
+    details_5.style.left = 300 + 'px'
+});
+var switch_6 = document.querySelector('.pop_ban_window_6');
+var details_6 = document.querySelector('.pop_ban_details_6');
+var back_6 = document.querySelector('.gp_feature_back_6');
+switch_6.addEventListener('click', function() {
+    details_6.style.left = 0 + 'px'
+});
+back_6.addEventListener('click', function() {
+    details_6.style.left = 300 + 'px'
+});
+var switch_7 = document.querySelector('.pop_ban_window_7');
+var details_7 = document.querySelector('.pop_ban_details_7');
+var back_7 = document.querySelector('.gp_feature_back_7');
+switch_7.addEventListener('click', function() {
+    details_7.style.top = 0 + 'px'
+});
+back_7.addEventListener('click', function() {
+    details_7.style.top = 250 + 'px'
+})
